da3bc108fe56231b703f32c39b8cf58c6980ab01
Dawid Ugniewski <80782@student.pb.edu.pl>
2023-11-27 19:57:26 +0100

    Poprawiona baza danych + AdminPanelController (Mockup) + KontaController (mockup)

---
+++ Brutus/Controllers/AdminPanelController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Brutus.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Konta()
+        {
+            return RedirectToAction("Index", "Konta");
+        }
+        public IActionResult Predmioty()
+        {
+            return RedirectToAction("Index", "Konta");
+        }
+    }
+}
---
+++ Brutus/Controllers/KontaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Brutus.Controllers
+{
+    public class KontaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Read()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
