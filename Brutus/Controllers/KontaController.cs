43fd526d26c8d50d0ad9f72991f3d1fe851203ab
Dawid Ugniewski <80782@student.pb.edu.pl>
2023-11-28 04:43:13 +0100

    AdminPanel + KontaController z przechwytywaniem ID i typu tworzonego konta

--- Brutus/Controllers/KontaController.cs
+++ Brutus/Controllers/KontaController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Brutus.Data;
+using Brutus.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Brutus.Controllers
 {
     public class KontaController : Controller
     {
+        private BrutusContext _context;
+        public KontaController(BrutusContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -11,6 +20,43 @@ namespace Brutus.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        //string Imie, string Nazwisko, string Email, string SkrotHasla, int NrTelefonu, string TypKonta
+        /*Konto konto = new Konto();
+        konto.Imie = Imie;
+        konto.Nazwisko = Nazwisko;
+        konto.Email = Email;
+        konto.SkrotHasla = SkrotHasla;
+        konto.NrTelefonu = NrTelefonu; */
+        [HttpPost]
+        public IActionResult Create(string Imie, string Nazwisko, string Email, string SkrotHasla, int NrTelefonu, string TypKonta)
+        {
+            //if (ModelState.IsValid)
+            //{
+            Konto konto = new Konto();
+            konto.Imie = Imie;
+            konto.Nazwisko = Nazwisko;
+            konto.Email = Email;
+            konto.SkrotHasla = SkrotHasla;
+            konto.NrTelefonu = NrTelefonu;
+            string typTworzonegoKonta = TypKonta;
+            int idTworzonegoKonta;
+
+            if (ModelState.IsValid)
+            {
+                _context.Konta.Add(konto);
+                _context.SaveChanges();
+                
+                idTworzonegoKonta = _context.Konta.OrderByDescending(x => x.ID_Konta).FirstOrDefault().ID_Konta;
+                
+                return RedirectToAction("Index", "Konta");
+            }
+            //}
+
+                return View(konto);
+
+            
         }
         public IActionResult Read()
         {
