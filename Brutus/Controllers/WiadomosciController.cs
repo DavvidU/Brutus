ddd635dec9f1da2510041e74c257aecfb0d31c5b
Dawid Ugniewski <80782@student.pb.edu.pl>
2024-01-30 07:37:08 +0100

    Możliwośc tworzenia zestawień: z zadanego okresu, dla całego przedmiotu. Wysyłanie wiadomości między nauczycielami a rodzicami.

--- Brutus/Controllers/WiadomosciController.cs
+++ Brutus/Controllers/WiadomosciController.cs
@@ -1,10 +1,12 @@
 ﻿using Brutus.Data;
+using Brutus.DTOs;
 using Brutus.Models;
 using Brutus.Services.Command;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace Brutus.Controllers
 {
@@ -29,23 +31,90 @@ namespace Brutus.Controllers
 
             if (idKonta == -1) { return NotFound(); }
 
+            /* Pobieranie wiadomosci, ktorych odbiorca jest uzytkownik */
+
             /* TU BYCMOZE TRZEBA UZYC .Include().ThenInclude().ToList() // OgloszeniaController:52 */
-            var wiadomosciZOdbiorcami = _context.KontaWiadomosci.Where(kw => kw.Odbiorca.ID_Konta == idKonta);
-            List<KontoWiadomosc> listaWiadomosciZOdbiorcami = wiadomosciZOdbiorcami.ToList();
+            List<KontoWiadomosc> polaczeniaWiadomosciOdebraneWiadomosc = _context.KontaWiadomosci.
+                                Include(w => w.Wiadomosc).Where(kw => kw.Odbiorca.ID_Konta == idKonta).ToList();
 
-            List<Wiadomosc> wiadomosciUzytkownika = new();
+            List<Wiadomosc> wiadomosciOdebrane = new();
 
-            foreach (var wiadomoscZOdbiarca in listaWiadomosciZOdbiorcami)
-                wiadomosciUzytkownika.Add(wiadomoscZOdbiarca.Wiadomosc);
+            foreach (var wiadomoscZOdbiarca in polaczeniaWiadomosciOdebraneWiadomosc)
+            {
+                wiadomosciOdebrane.Add(wiadomoscZOdbiarca.Wiadomosc);
+            }
 
-            return View(wiadomosciUzytkownika);
+            /* Pobieranie mozliwych adresatow dla uzytkownika */
+
+            List<Konto> mozliwiAdresaci = new();
+
+            if (User.IsInRole("Nauczyciel"))
+            {
+                mozliwiAdresaci = (from konto in _context.Konta
+                                   join rodzic in _context.Rodzice
+                                   on konto.ID_Konta equals rodzic.ID_Rodzica
+                                   select konto).ToList();
+            }
+            else if (User.IsInRole("Rodzic"))
+            {
+                mozliwiAdresaci = (from konto in _context.Konta
+                                   join nauczyciel in _context.Nauczyciele
+                                   on konto.ID_Konta equals nauczyciel.ID_Nauczyciela
+                                   select konto).ToList();
+            }
+
+            var viewModel = new WiadomosciWithMozliwiAdresaci
+            {
+                Wiadomosci = wiadomosciOdebrane,
+                MozliwiAdresaci = new SelectList(mozliwiAdresaci, "ID_Konta", "Nazwisko")
+            };
+
+            return View(viewModel);
         }
         [HttpPost]
-        public IActionResult Wyslij()
+        public IActionResult Wyslij(WiadomosciWithMozliwiAdresaci model)
         {
+            if (model.TrescWiadomosci == null || model.TrescWiadomosci.Length > 500)
+                return RedirectToAction("Index");
 
+            string userId = User.Identity.GetUserId();
 
-            return View();
+            var command = new TranslateIdCommand(userId, _context);
+            var invoker = new Invoker();
+            invoker.SetCommand(command);
+
+            int idKonta = invoker.Invoke();
+
+            if (idKonta == -1) { return NotFound(); }
+
+            /* Dodanie wiadomosci do bazy */
+
+            Konto kontoNadawcy = _context.Konta.FirstOrDefault(k => k.ID_Konta == idKonta);
+
+            Wiadomosc wiadomosc = new Wiadomosc
+            {
+                Nadawca = kontoNadawcy,
+                Data = DateTime.Now,
+                Tresc = model.TrescWiadomosci
+            };
+
+            _context.Wiadomosci.Add(wiadomosc);
+            _context.SaveChanges();
+
+            /* Powiazanie adresata z wiadomoscia w bazie */
+
+            Konto kontoOdbiorcy = _context.Konta.FirstOrDefault(k => k.ID_Konta == model.WybranyAdresatID);
+
+            KontoWiadomosc polaczanieWiadomoscOdbiorca = new KontoWiadomosc
+            {
+                Odbiorca = kontoOdbiorcy,
+                Wiadomosc = wiadomosc
+            };
+
+            _context.KontaWiadomosci.Add(polaczanieWiadomoscOdbiorca);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Brutus/DTOs/WiadomosciWithMozliwiAdresaci.cs
@@ -0,0 +1,13 @@
+﻿using Brutus.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Brutus.DTOs
+{
+    public class WiadomosciWithMozliwiAdresaci
+    {
+        public List<Wiadomosc> Wiadomosci { get; set; }
+        public SelectList MozliwiAdresaci { get; set; }
+        public int WybranyAdresatID { get; set; }
+        public string? TrescWiadomosci { get; set; }
+    }
+}
