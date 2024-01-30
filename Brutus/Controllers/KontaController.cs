06186a8d310d44e6cf23c7dbc6e79b9a5b9e253c
Adam Bagiński <80253@student.pb.edu.pl>
2024-01-30 11:09:18 +0100

    modyfikacja danych przez każdego użytkownika

--- Brutus/Controllers/KontaController.cs
+++ Brutus/Controllers/KontaController.cs
@@ -1,43 +1,51 @@
 ﻿using Brutus.Data;
 using Brutus.Models;
 using Brutus.Services;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+
 
 namespace Brutus.Controllers
 {
     public class KontaController : Controller
     {
+        private Microsoft.AspNetCore.Identity.UserManager<ApplicationUser> _userManager;
+        private Microsoft.AspNetCore.Identity.RoleManager<IdentityRole> _roleManager;
         private BrutusContext _context;
-        private UserManager<ApplicationUser> _userManager; /* Identity */
-        public KontaController(BrutusContext context, UserManager<ApplicationUser> userManager, 
-            RoleManager<IdentityRole> roleManager)
+        public KontaController(BrutusContext context, Microsoft.AspNetCore.Identity.UserManager<ApplicationUser> userManager, Microsoft.AspNetCore.Identity.RoleManager<IdentityRole> roleManager)
         {
             _context = context;
             _userManager = userManager;
+            _roleManager = roleManager;
         }
 
+        [Authorize(Roles = "Admin")]
         public IActionResult Index()
         {
             Read();
             return View();
         }
+        [Authorize(Roles = "Admin")]
         public IActionResult Create()
         {
             return View();
         }
         [HttpPost]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Create(string Imie, string Nazwisko, string Email, string SkrotHasla, int NrTelefonu, string TypKonta)
         {
             /* Konto - Identity */
             UserBuilder builder;
-            
+
             switch (TypKonta)
             {
                 case "Admin":
-                        builder = new AdminBuilder(_context);
-                        break;
+                    builder = new AdminBuilder(_context);
+                    break;
                 case "Uczen":
                     builder = new UczenBuilder(_context);
                     break;
@@ -67,7 +75,7 @@ namespace Brutus.Controllers
                 await _userManager.AddToRoleAsync(user, TypKonta);
 
                 if (ModelState.IsValid)
-                {                    
+                {
                     return RedirectToAction("Index");
                 }
             }
@@ -80,6 +88,7 @@ namespace Brutus.Controllers
             return View(przedmioty);
         }
         [HttpGet]
+        [Authorize(Roles = "Admin")]
         public IActionResult Update(int id)
         {
             var konto = _context.Konta.FirstOrDefault(k => k.ID_Konta == id);
@@ -89,16 +98,17 @@ namespace Brutus.Controllers
             return View(konto);
         }
         [HttpPost]
+        [Authorize(Roles = "Admin")]
         public IActionResult Update(Konto konto)
         {
 
             if (ModelState.IsValid)
             {
                 var existingKonto = _context.Konta.FirstOrDefault(k => k.ID_Konta == konto.ID_Konta);
-                
+
                 if (existingKonto == null)
                     return NotFound();
-                
+
                 existingKonto.Imie = konto.Imie;
                 existingKonto.Nazwisko = konto.Nazwisko;
                 existingKonto.Email = konto.Email;
@@ -113,11 +123,13 @@ namespace Brutus.Controllers
             return View(konto);
         }
         [HttpGet]
+        [Authorize(Roles = "Admin")]
         public IActionResult Delete()
         {
             return View();
         }
         [HttpPost]
+        [Authorize(Roles = "Admin")]
         public IActionResult Delete(int id)
         {
             var kontoDoUsuniecia = _context.Konta.FirstOrDefault(k => k.ID_Konta == id);
@@ -128,5 +140,42 @@ namespace Brutus.Controllers
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
+        [HttpGet]
+        public IActionResult UpdateForUser()
+        {
+            string userId = User.Identity.GetUserId();
+            var konto = _context.Konta.FirstOrDefault(k => k.ApplicationUserId == userId);
+
+            if (konto == null)
+                return NotFound();
+
+            return View(konto);
+        }
+        [HttpPost]
+        public IActionResult UpdateForUser(Konto konto)
+        {
+            string userId = User.Identity.GetUserId();
+
+            if (ModelState.IsValid)
+            {
+                var existingKonto = _context.Konta.FirstOrDefault(k => k.ApplicationUserId == userId);
+
+                if (existingKonto == null || existingKonto.ID_Konta != konto.ID_Konta)
+                    return RedirectToAction("AccesDenied", "Home");
+
+                existingKonto.Imie = konto.Imie;
+                existingKonto.Nazwisko = konto.Nazwisko;
+                existingKonto.Email = konto.Email;
+                existingKonto.SkrotHasla = konto.SkrotHasla;
+                existingKonto.NrTelefonu = konto.NrTelefonu;
+
+                _context.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(konto);
+        }
+
     }
 }
